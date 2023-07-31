d288e2a13b4898056b874380eec7b3b4bb9e4a9f
Yukta2784 <104755728+Yukta2784@users.noreply.github.com>
2023-07-31 22:26:17 +0530

    Create Automated_messages.py

---
+++ Automated_messages.py
@@ -0,0 +1,6 @@
+#first install pywhatkit 
+! pip install pywhatkit
+pywhatkit.sendwhatmsg_instantly(
+    phone_no='<enter the phone number to which you want to send message with country code in quotes>', 
+    message="Howdy! This message will be sent instantly!",
+)
